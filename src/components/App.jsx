617b57212f2982ca347642800f77bbe8548c2acd
TTanev <tomatanev@gmail.com>
2021-03-31 16:54:20 +0300

    image support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import "../styles/index.scss"
 import Recipes from "./Recipes"
+import sword from "../images/swc-sword.png"
+import swordSvg from "../images/sword.svg"
+
 const App = () => {
   return (
     <>
@@ -8,9 +11,10 @@ const App = () => {
         <section>
           <h1>Hello, React</h1>
         </section>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
+        <Recipes />
       </main>
-
-      <Recipes />
     </>
   )
 }
